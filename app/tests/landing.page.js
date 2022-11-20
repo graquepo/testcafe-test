28244d11a43a8285df0e17535cf5a85f06b840a9
graquepo <graquepo@hawaii.edu>
2022-11-20 12:40:57 -1000

    add test for ListStuff

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -9,8 +9,9 @@ class LandingPage {
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
     // This is first test to be run. Wait 10 seconds to avoid timeouts with GitHub Actions.
-    await testController.wait(10000).expect(this.pageSelector.exists).ok();
+    await testController.wait(70001).expect(this.pageSelector.exists).ok();
   }
+  /** Asserts that this page has table with at least 2 rows. */
 }
 
 export const landingPage = new LandingPage();
